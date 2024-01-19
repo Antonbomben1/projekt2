3ee165b0f1a134cc94e723e5d89590288cfeaff2
Anton Angervall <anton.angervall@elev.ga.ntig.se>
2024-01-19 12:29:23 +0100

    skapat en gubbe och backgrund

---
+++ main.py
@@ -0,0 +1,74 @@
+import pygame
+from sprites import *
+from config import *
+import sys
+#video 5 börja
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        self.character_spritesheet = Spritesheet('projekt2/img/character.png')
+        self.terrain_spritesheet = Spritesheet('projekt2/img/terrain.png')
+
+    def createTilemap(self):
+        for i, row in enumerate(tilemap):
+            for j, column in enumerate(row):
+                Ground(self, j, i)
+                if column == "B":
+                    Block(self, j, i)
+                if column == "P":
+                    Player(self, j, i)
+
+    def new(self):
+        # a new game starts
+        self.playing = True
+
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.blocks = pygame.sprite.LayeredUpdates()
+        self.enemies = pygame.sprite.LayeredUpdates()
+        self.attacks = pygame.sprite.LayeredUpdates()
+        self.createTilemap()
+
+    def events(self):
+        #game loop events
+        for event in pygame.event.get():
+           if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+
+    def update(self):
+        #game loop updates
+        self.all_sprites.update()
+
+    def draw(self):
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        self.clock.tick(FPS)
+        pygame.display.update()
+
+    def main(self):
+        # game loop
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        self.running = False
+
+    def game_over(self):
+        pass
+    def intro_screen(self):
+        pass
+
+
+g = Game()
+g.intro_screen()
+g.new()
+while g.running:
+    g.main()
+    g.game_over()
+
+pygame.quit()
+sys.exit
