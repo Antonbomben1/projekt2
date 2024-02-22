4559f8369bd580050fc64c9313f5ee512e4233d7
Anton Angervall <anton.angervall@elev.ga.ntig.se>
2024-02-22 16:11:56 +0100

    finish 1.1

--- main.py
+++ main.py
@@ -41,20 +41,20 @@ class Game:
         while i < 1:                
             spawn_x = random.randint(1, 18) 
             spawn_y = random.randint(1, 13) 
-            if tilemap[spawn_y][spawn_x] == "." and  tilemap[spawn_y][spawn_x] != "C" and tilemap[spawn_y][spawn_x] != "P": # Check if there is a player, coin or block on the tile
+            if tilemap[spawn_y][spawn_x] == "." and tilemap[spawn_y][spawn_x] != "P": # Check if there is a player or block on the tile
                 Coin(self, spawn_x , spawn_y) # Spawn a coin
                 i += 1
         self.enemy_spawn()
 
         # Spawn an enemy at a random location on the tilemap
     def enemy_spawn(self):
-        if self.score % 10 == 0 and self.score != 0: 
+        if self.score % 10 == 0 and self.score != 0: #spawning an enemy every 10 points
             i = 0
             while i < 1: 
-                spawn_x = random.randint(1, 18) 
+                spawn_x = random.randint(2, 17) 
                 spawn_y = random.randint(1, 13) 
-                if tilemap[spawn_y][spawn_x] == "." and  tilemap[spawn_y][spawn_x] != "C" and tilemap[spawn_y][spawn_x] != "P": # Check if there is a player, coin or block on the tile
-                    Enemy(self, spawn_x , spawn_y)
+                if tilemap[spawn_y][spawn_x] == "." and tilemap[spawn_y][spawn_x] != "P" and tilemap[spawn_y][spawn_x + 2] != "B" and tilemap[spawn_y][spawn_x - 2] != "B": # Check if there is a player or block within the 4 tiles he can move on on the tile
+                    Enemy(self, spawn_x , spawn_y) # Spawn an enemy
                     i += 1
 
         # Start a new game
